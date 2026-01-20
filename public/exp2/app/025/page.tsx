3dcf50d62dd21fa23b7adcad6b9285463c31f538
MaximillianNYC <max@mxmlln.com>
2026-01-19 23:15:34 -0500

    epx2/025 cleanup

--- public/exp2/app/025/page.tsx
+++ public/exp2/app/025/page.tsx
@@ -25,29 +25,6 @@ export default function Home() {
             scene="https://prod.spline.design/CevtQn0pvzPcYRxu/scene.splinecode" 
           />
         </div>
-        <div style={{
-          textAlign: 'center',
-          maxWidth: '600px',
-          padding: '0 24px'
-        }}>
-          <h1 style={{
-            color: '#ffffff',
-            fontSize: '48px',
-            fontWeight: '600',
-            margin: '0 0 16px 0',
-            lineHeight: '1.2'
-          }}>
-            Hello world.
-          </h1>
-          <p style={{
-            color: 'rgba(255, 255, 255, 0.7)',
-            fontSize: '20px',
-            margin: '0',
-            lineHeight: '1.6'
-          }}>
-            We are just testing some splines.
-          </p>
-        </div>
       </div>
     </main>
   );
