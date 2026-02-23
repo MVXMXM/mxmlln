acc7c3539d2f6cf62049b39723a1b6f4308d7a51
MaximillianNYC <max@mxmlln.com>
2026-02-22 22:23:48 -0800

    update middleware

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/portfolio.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
