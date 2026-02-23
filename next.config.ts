e38516b483c154455d4651faaf2ef3beae1a0d8b
MaximillianNYC <max@mxmlln.com>
2026-02-22 22:35:59 -0800

    redirect index to port

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,15 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
